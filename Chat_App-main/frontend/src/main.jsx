b21a93413b38ba366c6b851e23417f895906f0c8
Diganta-18 <digantab.cse.jisu21@gmail.com>
2026-03-21 01:24:32 +0530

    feat: add vercel analytics

--- Chat_App-main/frontend/src/main.jsx
+++ Chat_App-main/frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <AuthContextProvider>
         <SocketContextProvider>
           <App />
+          <Analytics />
         </SocketContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
