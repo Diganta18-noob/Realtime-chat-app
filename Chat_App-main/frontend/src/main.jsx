3a17c65d713c6373b4bec50decb3c9fb546b1ffa
Diganta-18 <digantab.cse.jisu21@gmail.com>
2026-03-21 01:28:43 +0530

    feat: add vercel speed-insights

--- Chat_App-main/frontend/src/main.jsx
+++ Chat_App-main/frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <SocketContextProvider>
           <App />
           <Analytics />
+          <SpeedInsights />
         </SocketContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
